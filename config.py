7e84ae7d3112ee6e73372c602f1c31b658e89b04
Aditya <adityaprabhat1998@gmail.com>
2020-01-07 20:16:05 +0530

    files added

---
+++ config.py
@@ -0,0 +1,17 @@
+#encode_images
+
+train_dir = r'C:\Anaconda\envs\deeplearning\projects\ImageCaptioning\dataset'
+transformed = r'C:\Anaconda\envs\deeplearning\projects\ImageCaptioning\transformed'
+batch_size = 1 
+
+#test_images
+test_file = r'C:\Anaconda\envs\deeplearning\projects\ImageCaptioning\text\Flickr_8k.testImages.txt'
+
+#text_data
+glove = r'C:\Anaconda\envs\deeplearning\Projects\ImageCaptioning\glove.6B.200d.txt'
+train_text_dir = r'C:\Anaconda\envs\deeplearning\projects\ImageCaptioning\text\Flickr_8k.trainImages.txt'
+dev_dir = r'C:\Anaconda\envs\deeplearning\projects\ImageCaptioning\text\Flickr_8k.devImages.txt'
+text_file = r'C:\Anaconda\envs\deeplearning\projects\ImageCaptioning\text\Flickr8k.token.txt'
+
+#model
+test_images_dir = 'C:\Anaconda\envs\deeplearning\Projects\ImageCaptioning\dataset\Flicker8k_Dataset'
